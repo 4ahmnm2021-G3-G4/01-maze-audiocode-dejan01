d811216f77a830bea7c1ea7abcebba13490c19eb
Dejan01 <dejan.klickovic01@htl-salzburg.ac.at>
2021-06-07 08:45:23 +0200

    Created Script which checks if the heavy or light sack is in scale

---
+++ Assets/MyGame/Scripts/Scale.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour
+{
+    public GameObject ScaleSack;
+    public GameObject Sacklight1;
+    public GameObject SackLight2;
+    public GameObject SackHeavy;
+
+    void OnTriggerEnter(Collider ScaleSack)
+    {
+        Debug.Log("Entered");
+
+        if(ScaleSack.gameObject == SackHeavy)
+        {
+            Debug.Log("Heavy Sack has entered");
+        }
+    }   
+     // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
